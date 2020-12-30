494e4792af2bb9c38e01e7ffae9a8e44d1ff05ae
unknown <hyder1@gmail.com>
2020-12-30 10:18:21 -0600

    removed node_modules

---
+++ .history/src/config_20201228105615.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DB_URL || process.env.TEST_DB_URL,
+  API_TOKEN: process.env.API_TOKEN || '5271f4f2-40cb-41be-adea-f38ae8395cc2',
+  JWT_SECRET: process.env.JWT_SECRET || 'grocery-list-api-jwt'
+  
+}
